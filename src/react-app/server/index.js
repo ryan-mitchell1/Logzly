e58fb08c58f9f941371f2b94a536d7bdd817b6d6
ryan-mitchell1 <52616063+ryan-mitchell1@users.noreply.github.com>
2021-02-06 14:59:37 -0700

    hello world program in node.js and react (#7)

---
+++ src/react-app/server/index.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const express = require("express");
+const app = express(); // create express app
+
+// add middlewares
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+// start express server on port 5000
+app.listen(5000, () => {
+    console.log("server started on port 5000");
+});
