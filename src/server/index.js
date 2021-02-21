d57fbec350e49c1ff3dd3488e6189242aefc5b0b
Nikesh Parajuli <47830777+nik-esh@users.noreply.github.com>
2021-02-20 22:43:45 -0700

    Testing lab (#13)
    * Added few tests and modified test.sh to include the commands to run them
    
    * Removed TODO file for the test lab

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,15 @@ const app = express(); // create express app
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "build")));
 
+app.get('/test', (req, res) => {
+    res.status(200);
+    res.json({ working: true });
+    res.end();
+})
+
 // start express server on port 5000
-app.listen(5000, () => {
+var server = app.listen(5000, () => {
     console.log("server started on port 5000");
 });
+
+module.exports = server
--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-exit 127
+yarn --cwd "src/server/tests" test
+
+exit 0
