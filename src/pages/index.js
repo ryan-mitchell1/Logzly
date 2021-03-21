306cd1ac34ab74204fcea468c03b8efc32bc5c5b
Nikesh Parajuli <nikeshparajuli@u.boisestate.edu>
2021-03-21 13:48:16 -0600

    Homepage UI enhancements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { AppBar, Button, Typography } from '@material-ui/core'
+import { AppBar, Box, Button, Typography } from '@material-ui/core'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Head from 'next/head'
 import Image from 'next/image'
@@ -24,16 +24,23 @@ export default function Home() {
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
 
-      <Image src="/home.svg" layout="fill"></Image>
+      <Image src="/home.svg" layout="fill" className="app-background" />
 
       <ThemeProvider theme={theme}>
         <div className="flex flex-row nav-div">
           <div className="flex flex-row icon-div">
-            <Image src="/favicon.ico" height="40" width="40" className="logo"></Image>
-            <Typography variant="h6" className="app-name">Logzly</Typography>
+            <Image src="/appicon.svg" height="40" width="40" className="logo" />
+            <Typography variant="h5" className="app-name" style={{ fontFamily: 'Courier' }}>Logzly</Typography>
           </div>
           <Button variant="contained" color="primary" className="home-login-button">Login</Button>
         </div>
+
+        <div className="app-about">
+          <Typography variant="h2" style={{ fontSize: '7vh', fontFamily: 'Courier', padding: '2vh 2vh' }}>A journaling app for developers.</Typography>
+          <Button variant="contained" color="secondary">Get Started</Button>
+        </div>
+
+
       </ThemeProvider>
     </div>
   )
