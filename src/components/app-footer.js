b432b1f9254d1d5fed681f3f4c0ee4d9aa1f0692
Nikesh Parajuli <nikeshparajuli@u.boisestate.edu>
2021-03-25 21:11:39 -0600

    Added hyperlink to Vercel banner on the site

--- src/components/app-footer.js
+++ src/components/app-footer.js
@@ -3,7 +3,9 @@ import Image from 'next/image'
 export default function AppFooter() {
     return (
         <div className="app-footer">
-            <Image src="/powered-by-vercel.svg" height="120" width="120" priority />
+            <a href="https://vercel.com/?utm_source=logzly&utm_campaign=oss">
+                <Image src="/powered-by-vercel.svg" height="120" width="120" priority />
+            </a>
         </div>
     )
 }
