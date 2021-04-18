6da0af70cb26bad5d0e489ca89a3eb3290807cf1
ryan-mitchell1 <ryanmitchell9@u.boisestate.edu>
2021-04-18 17:04:33 -0600

    getGroups from firebase

--- src/src/components/GroupPage.js
+++ src/src/components/GroupPage.js
@@ -13,72 +13,116 @@ import Button from '@material-ui/core/Button';
 import { useAuth } from "../lib/auth";
 import { Redirect } from "react-router";
 import { Link } from "react-router-dom";
+import Modal from "@material-ui/core/Modal";
+import { getGroups } from '../lib/db'
 
-const test_username = "mem3";
-
-function OptionButton(props) {
-    const isAdmin = props.admin == test_username;
-    const groupId = props.groupId;
-    if (isAdmin) {
-        return <Button variant="contained" color="secondary" size="small">Delete</Button>;
-    }
-    return <Button variant="contained" color="secondary" size="small">Leave</Button>;
-};
-
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     table: {
         width: '75%',
         marginLeft: '12.5%',
+        marginTop: '3%',
     },
     buttonStyle: {
         background: '#70B657'
     },
-});
+    modal: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+        top: `50%`,
+        left: `50%`,
+        transform: `translate(-50%, -50%)`,
+      },
+}));
+
 export default function GroupPage() {
     const auth = useAuth();
     const classes = useStyles();
 
     const [groupData, setGroupData] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [id, setId] = useState();
+    const [type, setType] = useState();
+
+    function body() {
+        return (
+        <div className={classes.modal}>
+          <h2 id="simple-modal-title" style={{ textAlign: "center", fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif" }}>Are you sure you want to {type} this group?</h2>
+            <div style={{textAlign:'center'}}>
+            <Button
+              style={{marginRight: '10%'}}
+              variant="contained"
+              color="primary"
+              onClick={() => leaveDelete(id, type)}
+            >
+              {type}
+                </Button>
+            <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => leaveDelete(id, type)}
+        >
+            Cancel
+            </Button>
+            </div>
+        </div>
+        )
+    };
 
     useEffect(() => {
-        var group_data = [
-            {
-                "groupId": 1,
-                "groupName": "group1",
-                "groupTitle": "Group 1 Title",
-                "groupMembers": [
-                    "mem1",
-                    "mem2",
-                    "mem3"
-                ],
-                "admin": "mem1",
-                "lastUpdated": "03-30-2020 12:22:45"
-            },
-            {
-                "groupId": 2,
-                "groupName": "group2",
-                "groupTitle": "Group 2 Title",
-                "groupMembers": [
-                    "mem1",
-                    "mem2",
-                    "mem3"
-                ],
-                "admin": "mem3",
-                "lastUpdated": "03-31-2020 14:26:45"
-            }
-        ];
-        setGroupData(group_data);
+        if(auth.user){
+            var g = getGroups(auth.user.uid);
+            g.then( data => {
+                setGroupData(data);
+            })
+        }
     }, [])
+    
+    const handleCloseCreate = () => {
+        setShowModal(false);
+    };
 
-    return auth.user ? (
-        <div>
-            <Navbar />
-            <TableContainer component={Paper} className={classes.table}>
+    const confirm = (type, id) => {
+        setShowModal(true);
+        setType(type);
+        setId(id);
+    };
+
+    function leaveDelete(id, type) {
+        if(type == 'delete'){
+
+        } else if(type == 'leave'){
+
+        }
+        setShowModal(false);
+    }
+
+    function OptionButton(props) {
+        var isAdmin = false;
+        if(auth.user){
+            isAdmin = props.admin == auth.user.uid;
+        } 
+        const id = props.id;
+        if (isAdmin) {
+            return <Button variant="contained" color="secondary" size="small" onClick={() => confirm('delete', id)}>Delete</Button>;
+        }
+        return <Button variant="contained" color="secondary" size="small" onClick={() => confirm('leave', id)}>Leave</Button>;
+    };
+
+    function GroupTable() {
+        if(groupData.length == 0){
+            return <h2 style={{ textAlign: "center", fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif" }}>You currently have no groups.</h2>
+        } else{
+            return (
+                <TableContainer component={Paper} className={classes.table}>
                 <Table aria-label="simple table">
                     <TableHead>
                         <TableRow>
                             <TableCell>Group Name</TableCell>
-                            <TableCell align="right">Group Title</TableCell>
+                            <TableCell align="center">Group Title</TableCell>
                             <TableCell align="center">Last Updated</TableCell>
                             <TableCell align="center">Leave/Delete</TableCell>
                             <TableCell align="center">Logs</TableCell>
@@ -86,15 +130,15 @@ export default function GroupPage() {
                     </TableHead>
                     <TableBody>
                         {groupData.map((row) => (
-                            <TableRow key={row.groupId}>
+                            <TableRow key={row.id}>
                                 <TableCell component="th" scope="row">
                                     {row.groupName}
                                 </TableCell>
-                                <TableCell align="right">{row.groupTitle}</TableCell>
+                                <TableCell align="center">{row.groupTitle}</TableCell>
                                 <TableCell align="center">{row.lastUpdated}</TableCell>
-                                <TableCell align="center"><OptionButton admin={row.admin} groupId={row.groupId} /></TableCell>
+                                <TableCell align="center"><OptionButton admin={row.admin} id={row.id} /></TableCell>
                                 <TableCell align="center">
-                                    <Link to={"/logs/" + row.groupId} style={{ textDecoration: 'none' }}>
+                                    <Link to={"/logs/" + row.id} style={{ textDecoration: 'none' }}>
                                         <Button variant="contained" color="primary" size="small">Go</Button>
                                     </Link>
 
@@ -104,6 +148,22 @@ export default function GroupPage() {
                     </TableBody>
                 </Table>
             </TableContainer>
+            )
+        }
+    }
+
+    return auth.user ? (
+        <div>
+            <Navbar />
+            <GroupTable />
+            <Modal
+                open={showModal}
+                onClose={handleCloseCreate}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+            {body()}
+            </Modal>
         </div>
     ) : (
         <Redirect to="/login" />
--- src/src/lib/db.js
+++ src/src/lib/db.js
@@ -12,3 +12,23 @@ export function createUser(uid, data) {
         .doc(uid)
         .set({ uid, ...data }, { merge: true })
 }
+
+export async function getGroups(uid) {
+    const groups = await firestore.collection('groups');
+
+    const promise1 = new Promise((resolve, reject) => {
+        groups.get().then((querySnapshot) => {
+            const tempDoc = []
+            querySnapshot.forEach((doc) => {
+                var group = { id: doc.id, ...doc.data() };
+                group.groupMembers.forEach((memberId) => {
+                    if(memberId == uid){
+                        tempDoc.push({ id: doc.id, ...doc.data() });
+                    }
+                })
+            })
+            resolve(tempDoc)
+        })
+    })
+    return promise1;
+}
