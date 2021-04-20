6047266c4a6a23e6257fa9d2d3c8c31903f682d7
ryan-mitchell1 <52616063+ryan-mitchell1@users.noreply.github.com>
2021-04-19 21:57:47 -0600

    create group complete (#26)

--- src/src/lib/db.js
+++ src/src/lib/db.js
@@ -1,4 +1,5 @@
 import firebase from './firebase'
+import md5 from 'crypto-js/md5';
 
 const firestore = firebase.firestore()
 
@@ -13,6 +14,31 @@ export function createUser(uid, data) {
         .set({ uid, ...data }, { merge: true })
 }
 
+export function createGroup(uid, groupName, groupTitle, password) {
+    var groupNameCheck = checkGroupName(groupName);
+
+    const promise1 = new Promise((resolve, reject) => {
+        groupNameCheck.then(data => {
+            if (data == true) {
+                resolve("taken");
+            } else {
+                firestore.collection('groups').add({
+                    "admin": uid,
+                    "groupMembers": [
+                        uid
+                    ],
+                    "groupName": groupName,
+                    "groupTitle": groupTitle,
+                    "password": "" + md5(password),
+                    "lastUpdated": getNow()
+                })
+                resolve("created");
+            }
+        })
+    })
+    return promise1;
+}
+
 export async function getGroups(uid) {
     const groups = await firestore.collection('groups');
 
@@ -22,7 +48,7 @@ export async function getGroups(uid) {
             querySnapshot.forEach((doc) => {
                 var group = { id: doc.id, ...doc.data() };
                 group.groupMembers.forEach((memberId) => {
-                    if(memberId == uid){
+                    if (memberId == uid) {
                         tempDoc.push({ id: doc.id, ...doc.data() });
                     }
                 })
@@ -31,4 +57,26 @@ export async function getGroups(uid) {
         })
     })
     return promise1;
+}
+
+async function checkGroupName(groupName) {
+    const groups = await firestore.collection('groups');
+
+    const promise1 = new Promise((resolve, reject) => {
+        groups.get().then((querySnapshot) => {
+            const tempDoc = []
+            querySnapshot.forEach((doc) => {
+                var group = { id: doc.id, ...doc.data() };
+                if (group.groupName == groupName) {
+                    resolve(true);
+                }
+            })
+            resolve(false)
+        })
+    })
+    return promise1;
+}
+
+function getNow(){
+    return new Date().getTime();
 }
